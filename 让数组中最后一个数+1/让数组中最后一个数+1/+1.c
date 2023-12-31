165e672b6d1602fbe53ac97c0b372c7f63f11cbf
Arthur <2606437441@qq.com>
2023-12-31 15:11:08 +0800

    更新了解决方法和图解

--- 让数组中最后一个数+1/让数组中最后一个数+1/+1.c
+++ 让数组中最后一个数+1/让数组中最后一个数+1/+1.c
@@ -1,7 +1,8 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
 #include <malloc.h>
-int* plus(int* parr,int sz)
+//错误写法，个位数时会越界
+/*int* plus(int* parr, int sz)
 {
 	int i = 0;
 	//计算总共需要动态分配的空间大小
@@ -42,5 +43,47 @@ int main()
 	{
 		printf("%d", ret[i]);
 	}
+	return 0;
+}*/
+
+
+
+//正确写法
+void plus(int* pa, int sz)
+{
+	int i = 0;
+	//从后向前遍历数组
+	for (i = sz - 1;i >= 0;i--)
+	{
+		//如果从最后一位以及前面有连续的9
+		//置0来完成进位
+		if (pa[i] == 9)
+		{
+			pa[i] = 0;
+		}
+		//不是9直接+1并结束进位
+		else if (pa[i] != 9)
+		{
+			pa[i] += 1;
+			break;
+		}
+	}
+}
+void output(int* pa, int sz)
+{
+	int i = 0;
+	for (i = 0;i < sz;i++)
+	{
+		printf("%d ", pa[i]);
+	}
+}
+int main()
+{
+	int a[4] = { 4,9,9,9 };
+	int sz = sizeof(a) / sizeof(a[0]);
+	//加一
+	plus(a, sz);
+	//打印
+	output(a, sz);
 	return 0;
 }
